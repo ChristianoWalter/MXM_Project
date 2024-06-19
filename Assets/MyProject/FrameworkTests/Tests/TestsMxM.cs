2d5e391d51f545c895df97dd662289d68941363f
Christiano Walter <chris.w.lemos03@gmail.com>
2024-06-19 19:37:14 -0300

    update manager

---
+++ Assets/MyProject/FrameworkTests/Tests/TestsMxM.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class TestsMxM
+{
+    // A Test behaves as an ordinary method
+    [Test]
+    public void TestsMxMSimplePasses()
+    {
+        // Use the Assert class to test conditions
+
+    }
+
+    [Test]
+    public void TestPlayerHealth()
+    {
+        
+    }
+
+    // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+    // `yield return null;` to skip a frame.
+    [UnityTest]
+    public IEnumerator TestsMxMWithEnumeratorPasses()
+    {
+        // Use the Assert class to test conditions.
+        // Use yield to skip a frame.
+        yield return null;
+    }
+}
