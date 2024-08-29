799714c5fd64b4920cc72ceda92869dcbd286b26
Christiano Walter <chris.w.lemos03@gmail.com>
2024-08-29 18:25:24 -0300

    Playfab Update

--- Assets/MyProject/Scripts/MultiplayerScripts/PlayfabManager.cs
+++ Assets/MyProject/Scripts/MultiplayerScripts/PlayfabManager.cs
@@ -27,7 +27,6 @@ public class PlayfabManager : MonoBehaviour
     [Header("Login Information")]
     [SerializeField] TMP_InputField usernameEmailLoginInput;
     [SerializeField] TMP_InputField passwordLoginInput;
-    [SerializeField] Button stillLogedBtn;
 
     [Header("Create Account Information")]
     [SerializeField] TMP_InputField usernameInput;
@@ -49,258 +48,8 @@ public class PlayfabManager : MonoBehaviour
         ShowScreens(1);
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        //Login();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
-    /*void SetUserData()
-    {
-        PlayFabClientAPI.UpdateUserData(new UpdateUserDataRequest()
-        {
-            Data = new Dictionary<string, string>()
-            {
-                {"XP", "500"},
-                {"Level", "2" }
-            }
-        },
-            result =>
-            {
-                Debug.Log("dados atualizados");
-            },
-            error =>
-            {
-                Debug.Log(error.ErrorMessage);
-            }
-
-        );
-    }
-
-    void GetUserData(string myPlayfabID)
-    {
-        PlayFabClientAPI.GetUserData(new GetUserDataRequest()
-        {
-            PlayFabId = myPlayfabID,
-            Keys = null
-        },
-            result =>
-            {
-                if (result == null || !result.Data.ContainsKey("XP"))
-                {
-                    Debug.Log("Sem chave");
-                }
-                else
-                {
-                    Debug.Log(result.Data["XP"].Value);
-                    Debug.Log(result.Data["Level"].Value);
-                }
-            },
-            error =>
-            {
-
-            }
-             );
-    }
-
-    public void ClienGetTittleData()
-    {
-        PlayFabClientAPI.GetTitleData(new GetTitleDataRequest(),
-            result => {
-                if (result == null || !result.Data.ContainsKey("FirstMessage"))
-                {
-                    Debug.Log("Sem mensagem");
-                }
-                else Debug.Log(result.Data["FirstMessage"]);
-            },
-            erro => {
-
-            }
-        );
-    }
-
-
-    #region Login
-    void LoginWithCustomID()
-    {
-        var requestLogin = new LoginWithCustomIDRequest
-        {
-            CustomId = SystemInfo.deviceUniqueIdentifier,
-            CreateAccount = true
-        };
-
-        PlayFabClientAPI.LoginWithCustomID(requestLogin, OnLoginSuccess, OnLoginFailed);
-
-
-    }
-
-    private void OnLoginFailed(PlayFabError error)
-    {
-        Debug.Log(error.ErrorMessage);
-    }
-
-    private void OnLoginSuccess(LoginResult result)
-    {
-        Debug.Log("Login efetuado");
-        PlayfabID = result.PlayFabId;
-        Debug.Log(result.PlayFabId);
-
-        PlayerInfo _playerInfo = new PlayerInfo("chris", 1, 0);
-        string _playerData = JsonUtility.ToJson(_playerInfo);
-
-        //GetUserDataWithJson(PlayfabID, "PlayerInfo");
-
-        GetAllPlayerData();
-
-        //SetUserDataWithJson("PlayerInfo", _playerData);
-
-        //ClienGetTittleData();
-        //GetUserData(PlayfabID);
-    }
-    #endregion
-
-    public void SavePlayerData(string _dataName, string _data)
-    {
-        PlayFabClientAPI.UpdateUserData(new UpdateUserDataRequest()
-        {
-            Data = new Dictionary<string, string>()
-            {
-                {_dataName, _data}
-        }
-        },
-            result =>
-            {
-                Debug.Log("Dados " + _dataName + " atualizados");
-            },
-            error =>
-            {
-                Debug.Log(error.ErrorMessage);
-            }
-
-        );
-    }
-
-    void GetAllPlayerData()
-    {
-        var _request = new GetUserDataRequest()
-        {
-            PlayFabId = PlayfabID,
-            Keys = null
-        };
-        PlayFabClientAPI.GetUserData(_request, GetAllPlayerDataSuccess, GetAllPlayerDataFail);
-    }
-
-    private void GetAllPlayerDataSuccess(GetUserDataResult result)
-    {
-        //playerData = result.Data;
-        Debug.Log("Datos do PlayerData recuperados");
-        Debug.Log(playerData.Count);
-
-        foreach (var item in result.Data)
-        {
-            playerData.Add(item.Key, item.Value.Value);
-        }
-
-        foreach (var item in playerData)
-        {
-            Debug.Log(item.Key + "-" + item.Value);
-        }
-
-    }
-
-    private void GetAllPlayerDataFail(PlayFabError error)
-    {
-
-    }*/
-
-
+    //Região destinada a métodos de login e criação de conta
     #region Create account/login in playfab game
-    public void CreateAccount(string username, string email, string password)
-    {
-        var registerRequest = new RegisterPlayFabUserRequest()
-        {
-            Username = username,
-            Email = email,
-            Password = password
-        };
-        PlayFabClientAPI.RegisterPlayFabUser(registerRequest,
-            result =>
-            {
-                ShowMessage("Conta criada com sucesso");
-                ShowScreens(1);
-                NetworkManager.instance.LoadScreen(9);
-            },
-            error =>
-            {
-                ShowMessage(error.ErrorMessage);
-                ShowScreens(2);
-                NetworkManager.instance.LoadScreen(9);
-                ShowMessage(error.ErrorMessage);
-            }
-            );
-    }
-
-    public void UserLogin(string _username, string _password)
-    {
-        var _request = new LoginWithPlayFabRequest()
-        {
-            Username = _username,
-            Password = _password
-        };
-        PlayFabClientAPI.LoginWithPlayFab(_request, UserLoginSuccess, 
-             error =>
-        {
-            var _requestEmail = new LoginWithEmailAddressRequest()
-            {
-                Email = _username,
-                Password = _password
-            };
-            PlayFabClientAPI.LoginWithEmailAddress(_requestEmail, UserLoginSuccess, UserLoginFailed);
-        }
-        );
-    }
-
-    private void UserLoginSuccess(LoginResult result)
-    {
-        NetworkManager.instance.LoadScreen(2);
-        NetworkManager.instance.PhotonLogin(usernameEmailLoginInput.text);
-    }
-
-    private void UserLoginFailed(PlayFabError error)
-    {
-        NetworkManager.instance.LoadScreen(9);
-        ShowMessage("Falha ao efetuar login: " + $"Error {error.ErrorMessage}");
-    }
-
-    public void BtnLogin()
-    {
-        NetworkManager.instance.LoadScreen(2);
-
-        string _usernameOrEmail = usernameEmailLoginInput.text;
-        string _password = passwordLoginInput.text;
-
-        if (string.IsNullOrEmpty(_usernameOrEmail) || string.IsNullOrEmpty(_password))
-        {
-            ShowMessage("Preencha todos os campos");
-            NetworkManager.instance.LoadScreen(9);
-        }
-        else if (_usernameOrEmail.Length < 3)
-        {
-            ShowMessage("Dados do usuário inválidos");
-            NetworkManager.instance.LoadScreen(9);
-        }
-        else
-        {
-            UserLogin(_usernameOrEmail, _password);
-        }
-    }
-
     public void BtnCreateAccount()
     {
         NetworkManager.instance.LoadScreen(2);
@@ -343,21 +92,94 @@ public class PlayfabManager : MonoBehaviour
 
     }
 
+    public void CreateAccount(string username, string email, string password)
+    {
+        var registerRequest = new RegisterPlayFabUserRequest()
+        {
+            Username = username,
+            Email = email,
+            Password = password
+        };
+        PlayFabClientAPI.RegisterPlayFabUser(registerRequest,
+            result =>
+            {
+                ShowMessage("Conta criada com sucesso");
+                ShowScreens(1);
+                NetworkManager.instance.LoadScreen(9);
+            },
+            error =>
+            {
+                ShowMessage(error.ErrorMessage);
+                ShowScreens(2);
+                NetworkManager.instance.LoadScreen(9);
+                ShowMessage(error.ErrorMessage);
+            }
+            );
+    }
+
+    public void BtnLogin()
+    {
+        NetworkManager.instance.LoadScreen(2);
+
+        string _usernameOrEmail = usernameEmailLoginInput.text;
+        string _password = passwordLoginInput.text;
+
+        if (string.IsNullOrEmpty(_usernameOrEmail) || string.IsNullOrEmpty(_password))
+        {
+            ShowMessage("Preencha todos os campos");
+            NetworkManager.instance.LoadScreen(9);
+        }
+        else if (_usernameOrEmail.Length < 3)
+        {
+            ShowMessage("Dados do usuário inválidos");
+            NetworkManager.instance.LoadScreen(9);
+        }
+        else
+        {
+            UserLogin(_usernameOrEmail, _password);
+        }
+    }
+
+    public void UserLogin(string _username, string _password)
+    {
+        var _request = new LoginWithPlayFabRequest()
+        {
+            Username = _username,
+            Password = _password
+        };
+        PlayFabClientAPI.LoginWithPlayFab(_request, UserLoginSuccess, 
+             error =>
+        {
+            var _requestEmail = new LoginWithEmailAddressRequest()
+            {
+                Email = _username,
+                Password = _password
+            };
+            PlayFabClientAPI.LoginWithEmailAddress(_requestEmail, UserLoginSuccess, UserLoginFailed);
+        }
+        );
+    }
+
+    private void UserLoginSuccess(LoginResult result)
+    {
+        NetworkManager.instance.LoadScreen(2);
+        NetworkManager.instance.PhotonLogin(usernameEmailLoginInput.text);
+    }
+
+    private void UserLoginFailed(PlayFabError error)
+    {
+        NetworkManager.instance.LoadScreen(9);
+        ShowMessage("Falha ao efetuar login: " + $"Error {error.ErrorMessage}");
+    }
+
     public void BtnAnonimousLogin()
     {
         NetworkManager.instance.LoadScreen(2);
         NetworkManager.instance.PhotonLogin(anonimousUsernameInput.text);
     }
-
-    public void BtnStayLoged()
-    {
-        if (stillLogedBtn)
-        {
-
-        }
-    }
     #endregion
 
+    //Região destinada a configurar a conta
     #region Account Manager
     public void BtnRecoverAccount()
     {
@@ -406,6 +228,7 @@ public class PlayfabManager : MonoBehaviour
     }
     #endregion
 
+    //Região destinada à controle de telas de Playfab
     #region Screens controller
     public void ShowScreens(int screens)
     {
